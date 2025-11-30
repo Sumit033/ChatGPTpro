0fb0fca6e5476c58d9aee171d7ffc95380a840ac
Sumit Monpara <202001122@daiict.ac.in>
2025-11-30 10:27:36 +0000

    feat/version-2

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Hello")
-print("I am version 1.0.0")
+
+
+from machine import Pin
+import time
+
+# Onboard LED pin (Pico W uses GP25)
+led = Pin(25, Pin.OUT)
+
+print("I am version 2.0.0")
+
+print("Starting LED blink program...")
+
+while _ in range(2):
+    led.value(1)  # LED ON
+    time.sleep(0.5)
+    led.value(0)  # LED OFF
+    time.sleep(0.5)
